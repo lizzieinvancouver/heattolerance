9f731e55dcb3f7a93462ceb4f88c69f3e3cf6353
merrilln <Nicole@Nicoles-MacBook-Pro.local>
2017-03-21 21:11:48 -0400

    incomplete cleaning code

---
+++ analyses/cleaning.R
@@ -0,0 +1,49 @@
+## Started March 20 2017 ##
+## By Nicole ##
+
+##Cleaning for heattolerance graphs 
+
+## general housekeeping ##
+rm(list=ls())
+options(stringsAsFactors = FALSE)
+
+## libraries
+library(ggplot2)
+require(plyr); require(dplyr); require(tidyr)
+
+# Set working directory: 
+if(length(grep("Lizzie", getwd())>0)) {setwd("~/Documents/git/projects/vinmisc/heattolerance/analyses") 
+} else
+  setwd("/Users/Nicole/GitHub/heattolerance/analysis/")
+
+## get data
+chambdater <- read.csv("input/chamberobservations_grapes2016.csv", header=TRUE)
+nodespurdater <- read.csv("input/baselinespursize.csv", header=TRUE)
+dater <- read.csv("input/phenmoist_grapes2016.csv", header=TRUE)
+ids <- read.csv("input/VitisExpReps2.csv", header=TRUE)
+
+## change header names here
+names(ids)[names(ids)=="Row"] <- "RowNum"
+names(ids)[names(ids)=="Plant"] <- "Num"
+names(ids)[names(ids)=="Variety"] <- "Var"
+names(chambdater)[names(chambdater)=="Treatment"] <- "Treat"
+ids.sm <- subset(ids, select=c("RowNum", "Num", "Var"))
+
+# change a few things to numeric
+unique(chambdater$stem1_percflow) # hypens and no entry mean zero, so update
+chambdater$stem1_percflow[which(chambdater$stem1_percflow=="-")] <- 0
+chambdater$stem1_percflow[which(chambdater$stem1_percflow=="")] <- 0
+chambdater$stem1_percflow <- as.numeric(chambdater$stem1_percflow)
+chambdater$stem1_bagbuds[which(chambdater$stem1_bagbuds=="")] <- 0
+chambdater$stem1_bagbuds[which(chambdater$stem1_bagbuds=="-")] <- 0
+chambdater$stem2_bagbuds[which(chambdater$stem2_bagbuds=="")] <- 0
+chambdater$stem2_bagbuds[which(chambdater$stem2_bagbuds=="-")] <- 0
+
+# delete a couple random rows of data after checking what's in them
+unique(chambdater$X)
+unique(chambdater$X.1)
+dater$X <- NULL
+dater$X.1 <- NULL
+
+
+
