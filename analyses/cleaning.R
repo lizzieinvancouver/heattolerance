311c76834920cf687343c489443e3580ab2b14d0
merrilln <Nicole@Nicoles-MacBook-Pro.local>
2017-03-27 07:30:57 -0400

    more updates to cleaning file

--- analyses/cleaning.R
+++ analyses/cleaning.R
@@ -14,7 +14,7 @@ require(plyr); require(dplyr); require(tidyr)
 # Set working directory: 
 if(length(grep("Lizzie", getwd())>0)) {setwd("~/Documents/git/projects/vinmisc/heattolerance/analyses") 
 } else
-  setwd("/Users/Nicole/GitHub/heattolerance/analysis/")
+  setwd("/Users/Nicole/GitHub/heattolerance/analyses/")
 
 ## get data
 chambdater <- read.csv("input/chamberobservations_grapes2016.csv", header=TRUE)
@@ -38,6 +38,8 @@ chambdater$stem1_bagbuds[which(chambdater$stem1_bagbuds=="")] <- 0
 chambdater$stem1_bagbuds[which(chambdater$stem1_bagbuds=="-")] <- 0
 chambdater$stem2_bagbuds[which(chambdater$stem2_bagbuds=="")] <- 0
 chambdater$stem2_bagbuds[which(chambdater$stem2_bagbuds=="-")] <- 0
+dater$EL_stem1[which(dater$EL_stem1=="0")] <- NA
+dater$EL_stem2[which(dater$EL_stem2=="0")] <- NA
 
 # delete a couple random rows of data after checking what's in them
 unique(chambdater$X)
@@ -45,5 +47,18 @@ unique(chambdater$X.1)
 dater$X <- NULL
 dater$X.1 <- NULL
 
+## format date (see http://www.statmethods.net/input/dates.html)
+dater$Date <- as.Date(dater$Date, format="%m/%d/%Y")
+dater$days <- as.numeric(format(dater$Date, "%j"))-228 # 245 is around the start of September
 
+chambdater$Date <- as.Date(chambdater$Date, format="%m/%d/%Y")
+chambdater$days <- as.numeric(format(chambdater$Date, "%j"))-228 # 245 is around the start of September
+
+nodespurdater$Date <- as.Date(nodespurdater$Date, format="%m/%d/%Y")
+nodespurdater$days <- as.numeric(format(nodespurdater$Date, "%j"))-228 # 245 is around the start of September
+
+## join dfs
+chambdats <- join(chambdater, ids.sm, by=c("RowNum", "Num"))
+dats <- join(dater, ids.sm, by=c("RowNum", "Num"))
+nsdats <- join(nodespurdater, ids.sm, by=c("RowNum", "Num"))
 
